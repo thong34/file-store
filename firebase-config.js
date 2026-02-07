9626635543eff3b6ca0d92004a2adfcffe0025c9
thong34 <m-13712507@moe-dl.edu.my>
2026-02-07 14:28:34 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCVvIA-rpL7GZalBaK_LUAHayjTJD4RYYw",
+  authDomain: "file-storage-b265e.firebaseapp.com",
+  projectId: "file-storage-b265e",
+  storageBucket: "file-storage-b265e.firebasestorage.app",
+  messagingSenderId: "647098522409",
+  appId: "1:647098522409:web:40249f09737a883b6f3538",
+  measurementId: "G-VVKGWS0T2V"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const storage = firebase.storage();
+const auth = firebase.auth();
