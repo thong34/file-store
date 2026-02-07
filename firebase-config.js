f5ec07c31aaa8ab4982d3e9d2692690012921a14
thong34 <m-13712507@moe-dl.edu.my>
2026-02-07 15:44:53 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCVvIA-rpL7GZalBaK_LUAHayjTJD4RYYw",
   authDomain: "file-storage-b265e.firebaseapp.com",
   projectId: "file-storage-b265e",
-  storageBucket: "file-storage-b265e.firebasestorage.app",
+  storageBucket: "file-storage-b265e.appspot.com",
   messagingSenderId: "647098522409",
   appId: "1:647098522409:web:40249f09737a883b6f3538",
   measurementId: "G-VVKGWS0T2V"
